863708beb1be141a61efda69bd5b412747298b5b
KingZoldack <jeanpaulismael@gmail.com>
2022-08-02 23:08:56 +0100

    Head and Direction
    Add sprite for snake head and modified direction so snake cannot go in the opposite direction unless turning first.

--- Assets/Scripts/References.cs
+++ Assets/Scripts/References.cs
@@ -7,4 +7,5 @@ public class References : MonoBehaviour
     public static string player = "Player";
     public static string food = "Food";
     public static string wall = "Wall";
+    public static string segment = "Segment";
 }
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
     Vector2 _direction = Vector2.right;
+
     List<Transform> _segments = new List<Transform>();
     
     [SerializeField] Transform _segmentsPiece;
+    [SerializeField] int _initialSize = 3;
+
+    Vector3 Rdirection = new Vector3(0,0,0);
+    Vector3 Ldirection = new Vector3(0, 0, 180);
+    Vector3 Udirection = new Vector3(0, 0, 90);
+    Vector3 Ddirection = new Vector3(0, 0, -90);
+
+    [SerializeField] bool goingUp;
+    [SerializeField] bool goingDown;
+    [SerializeField] bool goingLeft;
+    [SerializeField] bool goingRight;
+
+
+
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        _segments.Add(this.transform);
+  
+        ResetGame();
     }
 
     // Update is called once per frame
@@ -20,19 +39,56 @@ public class Snake : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            _direction = Vector2.up;
+            goingRight = false;
+            goingLeft = false;
+
+            if(!goingDown)
+            {
+                goingUp = true;
+                _direction = Vector2.up;
+                this.transform.rotation = Quaternion.Euler(Udirection);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            _direction = Vector2.down;
+            goingRight = false;
+            goingLeft = false;
+
+            if (!goingUp)
+            {
+                goingDown = true;
+                _direction = Vector2.down;
+                this.transform.rotation = Quaternion.Euler(Ddirection);
+
+            }
+
+
         }
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            _direction = Vector2.left;
+            goingUp = false;
+            goingDown = false;
+
+            if (!goingRight)
+            {
+                goingLeft = true;
+                _direction = Vector2.left;
+                this.transform.rotation = Quaternion.Euler(Ldirection);
+            }
+            
+            
         }
         else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            _direction = Vector2.right;
+            goingUp = false;
+            goingDown = false;
+
+            if (!goingLeft)
+            {
+                goingRight = true;
+                _direction = Vector2.right;
+                this.transform.rotation = Quaternion.Euler(Rdirection);
+            }
         }
     }
 
@@ -59,11 +115,43 @@ public class Snake : MonoBehaviour
         _segments.Add(segment);
     }
 
+    private void ResetGame()
+    {
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        //for (int i = 1; i < this._initialSize; i++)
+        //{
+        //    _segments.Add(Instantiate(this._segmentsPiece));
+        //}
+
+        _direction = Vector2.right;
+        this.transform.position = Vector3.zero;
+        this.transform.rotation = Quaternion.Euler(Rdirection);
+        goingRight = true;
+        goingLeft = false;
+        goingDown = false;
+        goingUp = false;
+
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == References.food)
         {
             Grow();
         }
+
+        if (other.tag != References.food)
+        {
+            ResetGame();
+        }
     }
+
+    
 }
